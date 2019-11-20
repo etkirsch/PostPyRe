31bd9f1285b06bfa4211314a0972ccfcd2fff902
Evan Kirsch <etkirsch@gmail.com>
2019-11-19 19:21:17 -0500

    now capable of serving a built react app

--- webserver/app.py
+++ webserver/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build/static', template_folder="../build")
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 version = '0.0.1'
 
 @app.route('/', methods=['GET'])
 def app_root():
-    return version
+    return render_template('index.html')
