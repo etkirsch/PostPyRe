789ce872b9a444b3664b2ee273402698abe68bda
Evan Kirsch <etkirsch@gmail.com>
2019-11-19 19:49:29 -0500

    add support for prod environments not being silly

--- webserver/app.py
+++ webserver/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder='../build/static', template_folder="../build")
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 version = '0.0.1'
 
+IS_PROD_ENV = os.environ.get('IS_PROD_ENV', False)
+
 @app.route('/', methods=['GET'])
 def app_root():
+    if IS_PROD_ENV:
+        return 'Hello'
     return render_template('index.html')
